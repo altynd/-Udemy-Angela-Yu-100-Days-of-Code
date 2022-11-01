dd0b50962d5b45f140f6246f6708fa01aefb43bf
Сэр Дъмитрий <altynd@mail.ru>
2022-11-01 20:42:40 +0400

    day3 finish

--- 03 Day 3 - Beginner - Control Flow and Logical Operators/main.py
+++ 03 Day 3 - Beginner - Control Flow and Logical Operators/main.py
@@ -36,3 +36,71 @@
 #     print("obese")
 # else:
 #     print("clinic obese")
+
+# def leap():
+#     year = int(input("input year:"))
+#     if year % 400 == 0:
+#         print("LEAP")
+#     elif year % 100 == 0:
+#         print("NOT LEAP")
+#     elif year % 4 == 0:
+#         print("LEAP")
+#     else:
+#         print("NOT LEAP")
+#
+# while True:
+#     leap()
+
+
+# print("Welcome to Python Pizza Deliveries!")
+# size = input("What size pizza do you want? S M or L?")
+# add_peperoni = input("Do you want extra peperoni?")
+# extra_cheese = input("Do you want extra cheese: Y or N")
+#
+# price = 0
+#
+# if size == "S":
+#     price += 15
+# elif size == "M":
+#     price += 20
+# else:
+#     price += 25
+# if add_peperoni == "Y":
+#     if size == "S":
+#         price += 2
+#     else:
+#         price += 3
+# if extra_cheese == "Y":
+#     price +=1
+#
+# print(f"Your  final bill is: ${price}")
+
+
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name?\n")
+# name2 = input("What is their name?\n")
+#
+# name_togather = name1+name2
+#
+# score = 0
+#
+# for i in name_togather.lower():
+#     if i == 't' or i == 'r' or i == "u" or i == "e":
+#         score += 10
+#
+# for i in name_togather.lower():
+#     if i == 'l' or i == 'o' or i == "v" or i == "e":
+#         score += 1
+#
+# print(score)
+#
+# if score <10 or score >90:
+#     print(f"Your score is {score}, you go together like coke snd mentos")
+# elif score >40 and score<50:
+#     print(f"Your score is {score}, you are alright together")
+# else:
+#     print(f"Your score is {score}")
+
+
+
+
