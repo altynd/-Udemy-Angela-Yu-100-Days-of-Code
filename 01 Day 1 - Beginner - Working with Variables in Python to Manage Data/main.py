9ce420853b2f3704d9495d6701d514b8f4e3bca2
Сэр Дъмитрий <altynd@mail.ru>
2022-11-01 11:19:13 +0400

    day1

---
+++ 01 Day 1 - Beginner - Working with Variables in Python to Manage Data/main.py
@@ -0,0 +1,19 @@
+# print("Hello world!\nHello world!")
+# print("Hello" + " " + "Angela")
+# print("Hello " + input("What is your name?"))
+
+# name = input("What is your name?\n")
+# print(len(name))
+
+# a = input("a:")
+# b = input("b:")
+#
+# (a,b) = (b,a)
+#
+# print("a= " + a)
+# print("b= " + b)
+print("Welcome to the Band Name Generator")
+city = input("What`s name of the city you grew up in?:\n")
+pet = input("What`s your pet`s name?:\n" )
+print("Your band name could be " + city.title() + " " + pet.title())
+
