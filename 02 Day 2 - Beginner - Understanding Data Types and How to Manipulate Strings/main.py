7bd60c86541d1a2a15fec0552557b1ff5a6de98d
Сэр Дъмитрий <altynd@mail.ru>
2022-11-01 11:28:25 +0400

    day3 add

--- 02 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/main.py
+++ 02 Day 2 - Beginner - Understanding Data Types and How to Manipulate Strings/main.py
@@ -1,16 +1,35 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# two_digits_number = input("Input the number:\n")
+# first_digit = int(two_digits_number[0])
+# second_digit = int(two_digits_number[1])
+#
+# sum = first_digit + second_digit
+# print(sum)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# height = float(input("enter your height in m:"))
+# weight = int(input("enter your weight in kg:"))
+# bmi = weight/(height**2)
+# bmi_int = round(bmi,2)
+#
+# print(bmi_int)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# age = int(input("What is your current age?:"))
+# age_left = 90 - age
+# months = age_left*12
+# weeks = age_left*52
+# days = age_left*365
+#
+# print(f"You have {days}, {weeks} weeks and {months} months left")
+
+
+print("Welcome to the tip calculator.")
+bill = float(input("What is the total bill?:$\n"))
+people = int(input("How many people to split the bill?:\n"))
+tip = int(input("What percentage tip would you like to give?: 10, 12 or 15\n"))
+
+should_pay = (bill + bill * tip / 100) / people
+should_pay=round(should_pay,2)
+
+print(f"Each person should pay$: {should_pay}")
---
+++ 03 Day 3 - Beginner - Control Flow and Logical Operators/main.py
@@ -0,0 +1,38 @@
+# number = int(input("choose a number:\n"))
+# if number % 2 == 0:
+#     print("it  is even")
+# else:
+#     print("it is not even")
+
+
+# is_height = input("Your height is greater then 120cm?\n")
+# if is_height != "y":
+#     print("You can`t ride")
+# else:
+#     print("You can drive")
+#     age = int(input("How old are you?:\n"))
+#     if age < 12:
+#         print("your price $5")
+#     elif age <=18:
+#         print("your pruce $7")
+#     else:
+#         print("your price $12")
+
+
+# height = float(input("enter your height in m:"))
+# weight = int(input("enter your weight in kg:"))
+# bmi = weight/(height**2)
+# bmi_int = round(bmi,2)
+#
+# print(bmi_int)
+#
+# if bmi_int <18.5:
+#     print("You are underweight")
+# elif bmi_int <25:
+#     print("Normal weight")
+# elif bmi_int <30:
+#     print("overweight")
+# elif bmi_int < 35:
+#     print("obese")
+# else:
+#     print("clinic obese")
