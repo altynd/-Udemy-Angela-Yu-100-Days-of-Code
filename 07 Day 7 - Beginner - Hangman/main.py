096fd763fa3050337e6142e5e65940dfcbace3c1
Сэр Дъмитрий <altynd@mail.ru>
2022-11-02 12:03:01 +0400

    add lesson 8

--- 07 Day 7 - Beginner - Hangman/main.py
+++ 07 Day 7 - Beginner - Hangman/main.py
@@ -2,7 +2,35 @@ import random
 
 word_list = ["ardvark", "baboon", "camel"]
 
-word = random.choice(word_list)
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+display = []
+for letter in chosen_word:
+    display.append("_")
+
+lives = 6
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess the letter").lower()
+    for position in range(len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
+
+    print(display)
+
+    if guess not in chosen_word:
+        lives -=1
+        if lives == 0:
+            end_of_game=True
+            print("You Lose!")
+
+    if "_" not in display:
+        end_of_game = True
+        print("You Win!")
+
+
+
 
 
 
