3c1b71aad650a650908be294d7d19d3f1abbf6ea
Сэр Дъмитрий <altynd@mail.ru>
2022-11-04 16:47:48 +0400

    day 14 add

--- 12 Day 12  - Beginner - Scope & Number Guessing Game/main.py
+++ 12 Day 12  - Beginner - Scope & Number Guessing Game/main.py
@@ -1,11 +1,50 @@
+import random
+
+random_number = random.randint(1,100)
+
 print("Welcome to the Number Guessing Game!")
-print("i am thinking of a number between 1 and 100")
+print("I am thinking of a number between 1 and 100")
 difficulty = input("Choose a difficalty. Type 'easy' or 'hard'")
 
 if difficulty == "hard":
     remaining = 5
+    print(f"You have {remaining} attempts to guess the  number")
 else:
     remaining = 10
+    print(f"You have {remaining} attempts to guess the  number")
 
-print(f"You have {remaining} attemps to guess the  number")
-guess = input("Make a guess:")
+print(f"random number {random_number}")
+
+def guess_again():
+    global remaining
+    global game_continig
+    print("Guess again")
+    remaining -= 1
+    if remaining >0:
+        print(f"You have {remaining} attemps to guess the  number")
+    else:
+        print("You have run out of guesses, you lose")
+        game_continig = False
+
+def calculate():
+    global game_continig
+    if guess == random_number:
+        print("You win!")
+        game_continig = False
+        return
+    elif guess > random_number:
+        print("Too high")
+    elif guess < random_number:
+        print("Too low")
+    guess_again()
+
+
+game_continig = True
+while game_continig:
+    guess = int(input("Make a guess:"))
+    calculate()
+
+
+
+
+
---
+++ 14 Day 14 - Beginner - Higher Lower Game Project/data.py
@@ -0,0 +1,26 @@
+data = [
+    {
+        "name": "Instagram",
+        "follower_count":346,
+        "description":"Social media platform",
+        "country": "United States"
+    }
+    {
+        "name": "Cristiano Ronaldo",
+        "follower_count": 215,
+        "description": "Footballer",
+        "country": "Portugal"
+    }
+    {
+        "name": "Ariana grade",
+        "follower_count": 183,
+        "description": "Musican and actress",
+        "country": "United States"
+    }
+    {
+        "name": "Dwayne Johnson",
+        "follower_count": 181,
+        "description": "Actor and  professional",
+        "country": "United States"
+    }
+]
