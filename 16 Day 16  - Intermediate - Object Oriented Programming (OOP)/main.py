a50b23afd08e505f65b91dbeab1d2c575e6adfa3
Сэр Дъмитрий <altynd@mail.ru>
2022-11-05 20:30:19 +0400

    day 16 start

--- 16 Day 16  - Intermediate - Object Oriented Programming (OOP)/main.py
+++ 16 Day 16  - Intermediate - Object Oriented Programming (OOP)/main.py
@@ -1,16 +1,3 @@
-from menu import MENU
-
-
-
-def report():
-    print(
-    "Water: 300 ml\n" 
-    "Milk: 200ml\n" 
-    "Coffee: 100g\n" 
-    "Money: $0"
-    )
-
-while True:
-    coffee_choose = input("What would you like? (espresso/latte/cappuccino): ")
-    if coffee_choose == "report":
-        report()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
