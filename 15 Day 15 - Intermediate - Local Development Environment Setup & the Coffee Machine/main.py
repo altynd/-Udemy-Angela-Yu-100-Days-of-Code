e01060be07a92d844fd952568abcb460e1cf41dc
Сэр Дъмитрий <altynd@mail.ru>
2022-11-05 16:46:48 +0400

    day 15 end

--- 15 Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/main.py
+++ 15 Day 15 - Intermediate - Local Development Environment Setup & the Coffee Machine/main.py
@@ -1,13 +1,13 @@
-#ALL WORKING
-#Change coins
+# ALL WORKING
 
 from menu import MENU
 from menu import resources
 
-coffee_price = None
+
 menu = MENU
 
-#TODO-3
+
+# TODO-3
 def report():
     """prints resourses """
     for key in resources:
@@ -15,26 +15,27 @@ def report():
 
 
 def making(coffee_chosen):
+    enouth_resources = False
     for menu_item in menu:
         if menu_item == coffee_chosen:
-            #TODO-4
+            # TODO-4
             coffee_ingredients = menu[coffee_chosen]["ingredients"]
             for ingredient in coffee_ingredients:
-                if resources[ingredient] > coffee_ingredients[ingredient]:
+                if resources[ingredient] >= coffee_ingredients[ingredient]:
                     enouth_resources = True
                 else:
                     print(f"Sorry, Not anouth {ingredient}")
                     return
-            #TODO-5
+            # TODO-5
             coffee_price = menu[coffee_chosen]["cost"]
             money = coins()
-            #TODO-6
+            # TODO-6
             if money > coffee_price and enouth_resources:
                 resources["money"] = resources["money"] + coffee_price
-                change = round((money - coffee_price),2)
+                change = round((money - coffee_price), 2)
 
                 print(f"Here is ${change} in change")
-                #TODO-7
+                # TODO-7
                 for ingredient in coffee_ingredients:
                     resources[ingredient] = resources[ingredient] - coffee_ingredients[ingredient]
                 print(f"Here is your {coffee_chosen}, Enjoy!")
@@ -43,32 +44,29 @@ def making(coffee_chosen):
                 print(f"Here is your money: {money}")
                 print(f"Insert more coins {coffee_price}")
 
-#TODO-5
+
+# TODO-5
 def coins():
     print("Insert some coins")
-    quarters = int(input("How many quarters?"))*25/100
-    dimes = int(input("How many dimes?"))*10/100
-    nickels = int(input("How many nickels?"))*5/100
-    pennies = int(input("How many pennies?"))/100
-    money = quarters +dimes +nickels +pennies
+    quarters = int(input("How many quarters?")) * 25 / 100
+    dimes = int(input("How many dimes?")) * 10 / 100
+    nickels = int(input("How many nickels?")) * 5 / 100
+    pennies = int(input("How many pennies?")) / 100
+    money = quarters + dimes + nickels + pennies
     return money
+
 
 is_working = True
 while is_working:
-    #TODO-1
+    # TODO-1
     coffee_choosen = input("What would you like? (espresso/latte/cappuccino): ")
-    #TODO-2
+    # TODO-2
     if coffee_choosen == "off":
         is_working = False
         print("Coffe mashine OFF")
-    #TODO-3
+    # TODO-3
     elif coffee_choosen == "report":
         report()
-    #TODO-4 TODO-5 TODO-6 TODO-7
+    # TODO-4 TODO-5 TODO-6 TODO-7
     else:
         making(coffee_choosen)
-
-
-
-
-
